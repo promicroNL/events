6c8aff08688333e49f71d4b4538ba2a8ce027911
Tonie Huizer <tonie.huizer@promicro.nl>
2025-11-16 05:50:35 +0100

    Thank you Vancouver

---
+++ 2025-11-16 SQL Saturday Oregon SW Washington 2025/README.md
@@ -0,0 +1,2 @@
+# 2025-11-16 Saturday Oregon SW Washington
+Event materials - Thanks for coming.
---
+++ 2025-11-16 SQL Saturday Oregon SW Washington 2025/Two Developers One Mission Make a Test Database That Doesn’t Suck/002.Create Masking Config.ps1
@@ -0,0 +1,11 @@
+﻿$Instance = 'BRZ7JX3\SQL2025'
+$StageDb  = 'RetroPixelsProduction'
+
+$OutDir   = "C:Temp\\PII-Masking\$(Get-Date -Format yyyyMMdd-HHmm)"
+New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
+
+# Generate starter masking config (for finetuning)
+$ConfigPath = Join-Path $OutDir 'masking-config.json'
+New-DbaDbMaskingConfig -SqlInstance $Instance -Database $StageDb -Path $ConfigPath
+
+#https://docs.dbatools.io/bogus/
